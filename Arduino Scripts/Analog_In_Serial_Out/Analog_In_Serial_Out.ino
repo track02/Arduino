d75f8fe5c1a9d08c26180799017a9b76d81eb00a
Alex <a.clarke@runbox.com>
2020-10-22 14:55:11 +0100

    Sensor script wip

---
+++ Arduino Scripts/Analog_In_Serial_Out/Analog_In_Serial_Out.ino
@@ -0,0 +1,34 @@
+/*
+  Repeatedly listens for analog in values from designated pins
+  Every X seconds, packages latest reading set and transmits over serial
+*/
+
+//Sensor Pins
+int firstSensor = 0;    // first analog sensor
+int secondSensor = 1;   // second analog sensor
+int thirdSensor = 2;    // digital sensor
+
+//Sensor Readings
+float firstReading = 0.0;
+float secondReading = 0.0;
+float thirdReading = 0.0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  firstReading = analogRead(firstSensor);
+  secondReading = analogRead(secondSensor);
+  thirdReading = analogRead(thirdSensor);
+  
+  Serial.print(firstReading);
+  Serial.print(",");
+  Serial.print(secondReading);
+  Serial.print(",");
+  Serial.println(thirdReading);
+  delay(5000);
+  
+
+}
